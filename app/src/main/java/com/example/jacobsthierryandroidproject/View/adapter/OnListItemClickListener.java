3091faa116e92077bf113935f7f3bedae7ea7144
Thierry <thierry.jm35@gmail.com>
2020-06-14 12:14:42 +0200

    added logo

--- app/src/main/java/com/example/jacobsthierryandroidproject/View/adapter/OnListItemClickListener.java
+++ app/src/main/java/com/example/jacobsthierryandroidproject/View/adapter/OnListItemClickListener.java
@@ -2,6 +2,6 @@ package com.example.jacobsthierryandroidproject.View.adapter;
 
 import com.example.jacobsthierryandroidproject.Pojo.foodObjects.Recipe;
 
-public interface OnListItemClickListener{
+public interface OnListItemClickListener {
     void onClick(Recipe food);
 }
